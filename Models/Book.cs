5ba36d895db736ebb636cd32ca96c93e8907d0de
Aida Ardelean <renata.ardelean@yahoo.com>
2021-12-08 18:36:59 +0200

    categorii

--- Models/Book.cs
+++ Models/Book.cs
@@ -11,18 +11,24 @@ namespace Ardelean_Aida_Laborator8.Models
     {
         public int ID { get; set; }
 
-        [Display(Name = "Book Title")]
+        [Required, StringLength(150, MinimumLength = 3)]
+        [Display(Name =  "Book Title")]
         public string Title { get; set; }
+        [RegularExpression(@"^[A-Z][a-z]+\s[A-Z][a-z]+$", ErrorMessage = "Numele autorului trebuie sa fie de forma 'Prenume Nume'"), Required, StringLength(50, MinimumLength = 3)]
+
         public string Author { get; set; }
 
-        [Column(TypeName = "decimal(6, 2)")]
+        [Range(1, 300)]
+        [Column(TypeName = "decimal(6,2")]
         public decimal Price { get; set; }
 
         [DataType(DataType.Date)]
         public DateTime PublishingDate { get; set; }
+
         public int PublisherID { get; set; }
         public Publisher Publisher { get; set; }
-        public ICollection<BookCategory> BookCategories { get; set; }
-    } //navigation property
-}
 
+        public ICollection<BookCategory> BookCategories { get; set; }
+
+    }
+}
